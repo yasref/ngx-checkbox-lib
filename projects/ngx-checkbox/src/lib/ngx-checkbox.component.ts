1c7ee72f053779d0fb7c797103598d8528ef60e6
Yaseen Alrefaee <yaseenref@gmail.com>
2022-07-14 20:50:32 +0300

    NgxCheckboxComponent

--- projects/ngx-checkbox/src/lib/ngx-checkbox.component.ts
+++ projects/ngx-checkbox/src/lib/ngx-checkbox.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-checkbox',
-  template: `
-    <p>
-      ngx-checkbox works!
-    </p>
-  `,
+  selector: 'ngx-checkbox',
+  templateUrl: 'ngx-checkbox.component.html',
   styles: [
   ]
 })
 export class NgxCheckboxComponent implements OnInit {
 
   constructor() { }
+  @Input() color = '#3c3c3c';
+  @Input() backColor = '#fff';
+  @Input() borderColor = '1px rgba(0, 0, 0, 0.2) solid';
+  @Input() shadow = '0px 0px 4px 1px #6c6c6c';
 
+  @Input() cWidth = '20px';
+  @Input() cHeight = '20px';
+
+  @Input() checkWidth = '6px';
+  @Input() checkHeight = '10px';
+
+  @Input() value = true;
+  @Output() checkedChanged = new EventEmitter<any>();
   ngOnInit(): void {
   }
-
+  change($event: any) {
+    console.log('🚀 ~ file: ngx-checkbox.component.ts ~ line 27 ~ NgxCheckboxComponent ~ $event', $event.target.checked);
+  }
 }
